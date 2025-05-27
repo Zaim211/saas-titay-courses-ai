b9f0347a6b17ad9bfd8464ef02a09e18b85732d7
Zaim21 <yousseftitazaim@gmail.com>
2025-05-27 13:47:14 +0100

    initial setup app

---
+++ app/companious/new/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NewCompanious = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default NewCompanious
---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const Page = () => {
+  return (
+  <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
+      <h1>Welcome to my saas app</h1>
+      <Button>Click Me</Button>
+  </div>
+
+  )
+}
+
+export default Page
